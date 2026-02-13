a298b7e9d2314db9b3bc110d798673e1c6088e2b
netizen-om <borisagarom@gmail.com>
2026-02-13 23:40:06 +0530

    completed setup for credential based auth

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from '@/features/auth/components/register-form.tsx'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <RegisterForm />
+    </div>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,15 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import React, { Suspense } from "react";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+"use client";
 
-const page = async () => {
-  const queryClient = getQueryClient();
- 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
+const page = () => {
+  const { data } = authClient.useSession();
 
   return (
-    <div className="">
-      {/* {JSON.stringify(user)} */}
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>Loading....</p>}>
-
-        <Client/>
-        </Suspense>
-      </HydrationBoundary>
+    <div>
+      ({JSON.stringify(data)})
+      {data && <Button onClick={() => authClient.signOut()}>Logout</Button>}
     </div>
   );
 };
