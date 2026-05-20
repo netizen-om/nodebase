126ae47f6c77ac5116b978c75d0412adb597dfe4
Om Borisagar <borisagarom@gmail.com>
2026-05-20 11:44:33 +0530

    created basic tRPC routes for workflow CRUD

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,12 +1,26 @@
+import { WorkflowsList } from '@/features/workflows/components/workflows';
+import { prefetchlWorkflows } from '@/features/workflows/server/prefetch';
 import { requireAuth } from '@/lib/auth-utils'
-import React from 'react'
+import { HydrateClient } from '@/trpc/server';
+import React, { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary';
 
 const Page = async() => {
 
-  await requireAuth();
+  await requireAuth(); 
+
+  prefetchlWorkflows();
 
   return (
-    <div>Workflows</div>
+    <div>
+      <HydrateClient>
+        <ErrorBoundary fallback={<p>Error !!!</p>}>
+            <Suspense fallback={<p>Loading...</p>}>
+                <WorkflowsList />
+            </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </div>
   )
 }
 
