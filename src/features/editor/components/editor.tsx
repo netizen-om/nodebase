f80fe1c1632b37528b668caa954b7adeeaeb6feb
Om Borisagar <borisagarom@gmail.com>
2026-05-29 14:10:56 +0530

    Completed editor setup and added Initial node

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,7 +1,28 @@
 "use client"
 
 import { ErrorView, LoadingView } from "@/components/entity-components"
+<<<<<<< Updated upstream
+=======
+import { useState, useCallback } from 'react';
+import {
+    ReactFlow,
+    applyNodeChanges,
+    applyEdgeChanges,
+    addEdge,
+    type Node,
+    type Edge,
+    type NodeChange,
+    type EdgeChange,
+    type Connection,
+    Background,
+    Controls,
+    MiniMap,
+    Panel
+} from '@xyflow/react';
+>>>>>>> Stashed changes
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows"
+import { nodeComponents } from "@/config/node-components";
+import { AddNodeButton } from "./add-node-button";
 
 export const EditorLoading = () => {
     return <LoadingView message="Loading editor..." />
@@ -14,11 +35,56 @@ export const EditorError = () => {
 export const Editor = (
     { workflowId } : { workflowId : string }
 ) => {
+<<<<<<< Updated upstream
     const { data : workflow } = useSuspenseWorkflow(workflowId);
 
     return (
         <p>
             {JSON.stringify(workflow, null, 1)}
         </p>
+=======
+    const { data: workflow } = useSuspenseWorkflow(workflowId);
+
+    const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+    const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+
+    const onNodesChange = useCallback(
+        (changes: NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+        [],
+    );
+    const onEdgesChange = useCallback(
+        (changes: EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+        [],
+    );
+    const onConnect = useCallback(
+        (params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+        [],
+    );
+
+    return (
+        <div className="size-full">
+            <ReactFlow 
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView
+                nodeTypes={nodeComponents}
+                proOptions={{
+                    hideAttribution : true
+                }}
+            >
+                <Background />
+                <Controls />
+                <MiniMap />
+
+                <Panel position="top-right">
+                    <AddNodeButton />
+                </Panel>
+
+            </ReactFlow>
+        </div>
+>>>>>>> Stashed changes
     )
 }
