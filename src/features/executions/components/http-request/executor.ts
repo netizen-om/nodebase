def15c6141bfa7690b462f7b447cc3190fcb509a
Om Borisagar <borisagarom@gmail.com>
2026-07-13 22:31:29 +0530

    Added Handlebars Helper  to compile JSON while node execution

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -3,6 +3,14 @@ import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky"
 import Handlebars from "handlebars"
 
+Handlebars.registerHelper("json", (context) => {
+    const jsonString = JSON.stringify(context, null, 2)
+
+    const safeString = new Handlebars.SafeString(jsonString)
+
+    return safeString;
+})
+
 type HttpRequestData = {
     variableName: string;
     endpoint: string;
@@ -36,14 +44,19 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     const result = await step.run("http-request", async () => {
 
         const endpoint = Handlebars.compile(data.endpoint)(context);
-        console.log("ENDPOINT : ", endpoint);
         
         const method = data.method || "GET";
 
         const options: KyOptions = { method }
 
         if (["POST", "PUT", "PATCH"].includes(method)) {
-            options.body = data.body;
+            const resolved = Handlebars.compile(data.body || "{}")(context);
+            JSON.parse(resolved);
+
+            console.log("BODY : ", resolved);
+            
+
+            options.body = resolved;
             options.headers = {
                 "Content-Type": "application/json",
             }
